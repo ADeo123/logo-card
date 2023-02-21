4478a495ac5e2c8f0f2f6caa6e206582e47e1696
Ameya Deo <122508209+ADeo123@users.noreply.github.com>
2023-02-21 13:12:46 -0500

    Iteration 2
    All buttons perform their function.

--- logo-card2/src/logo-card2.js
+++ logo-card2/src/logo-card2.js
@@ -6,11 +6,15 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 class LogoCard2 extends LitElement {
   static properties = {
     header: { type: String },
+    title: {type: String},
   }
 
   static get styles(){
     return css`
     :host {
+
+    }
+
       .mainCard {
         text-align: center;
         border: 2px solid rgb(0, 0, 0);
@@ -34,12 +38,16 @@ class LogoCard2 extends LitElement {
         color: white;
       }
       .image {
-        padding: 8px 8px 32px 10px;
-        width: 100px;
-        
+        padding: 8px px 10px 10px;
+        width: 350px;
+        border-color: black;
+        border-width: 10px;
       }
       .heading{
         margin: 50px;
+      }
+      meme-maker{
+        width: 300px;
       }
       @media (min-width: 500px) and (max-width: 800px) {
         button {
@@ -53,19 +61,19 @@ class LogoCard2 extends LitElement {
           image-resolution: auto;
         }
       }
-    }
-  
+
   `};
 
   constructor() {
     super();
     this.header = 'My app';
+    this.title = "Pennsylvania State University"
   }
 
   render() {
     return html`
       <div class="mainCard">
-        <h1 class="heading"> Penn State University </h1>
+        <h1 class="heading">${this.title}</h1>
         <meme-maker alt="Penn State Logo" image-url="https://images.onwardstate.com/uploads/2014/02/NittanyLionLogo.jpg" width="300" top-text="We Are" bottom-text="Penn State" class="image"></meme-maker>
         <h2>Description</h2>
         <p>The Penn State University logo showing the Nittany Lion. </p>
