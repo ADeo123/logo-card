51c23c0f0d61b647f05aa66a25e64b9f42c43b8f
Ameya Deo <122508209+ADeo123@users.noreply.github.com>
2023-03-04 11:05:49 -0800

    Big Fixes

--- src/logo-card2.js
+++ src/logo-card2.js
@@ -7,12 +7,30 @@ class LogoCard2 extends LitElement {
   static properties = {
     header: { type: String },
     title: {type: String},
+    accentColor: {
+    type: String,
+    reflect: true,
+    attribute: 'accent-color'},
+    opened: {type: Boolean, reflect: true},
   }
 
   static get styles(){
     return css`
-    :host {
-
+    :host([accent-color="blue" ]) .mainCard {
+      background-color: var(--logo-card-accent-color, blue);
+      color: white; 
+    }
+    :host([accent-color="gray" ]) .mainCard {
+      background-color: var(--logo-card-accent-color, gray);
+      color: white; 
+    }
+    :host([accent-color="red" ]) .mainCard {
+      background-color: var(--logo-card-accent-color, red);
+      color: white; 
+    }
+    :host([accent-color="white" ]) .mainCard {
+      background-color: var(--logo-card-accent-color, white);
+      color: black; 
     }
       .mainCard {
         text-align: center;
@@ -68,6 +86,8 @@ class LogoCard2 extends LitElement {
     super();
     this.header = 'My app';
     this.title = "Pennsylvania State University"
+    this.accentColor = null;
+    this.opened = false;
   }
 
   render() {
@@ -76,7 +96,7 @@ class LogoCard2 extends LitElement {
         <h1 class="heading">${this.title}</h1>
           <meme-maker alt="Penn State Logo" image-url="https://images.onwardstate.com/uploads/2014/02/NittanyLionLogo.jpg" width="300" top-text="We Are" bottom-text="Penn State" class="image"></meme-maker>
           <h2>Description</h2>
-          <details>
+          <details> 
             <summary>More Information about Penn State University</summary>
             <slot> </slot></details>   
       </div>
